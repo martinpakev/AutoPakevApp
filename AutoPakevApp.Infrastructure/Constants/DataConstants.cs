00c874f4e4dff96f57d61ffb68837f009479d4cf
martinpakev <marti_pakev@abv.bg>
2025-02-19 13:05:48 +0200

    Brand model implemented

---
+++ AutoPakevApp.Infrastructure/Constants/DataConstants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoPakevApp.Infrastructure.Constants
+{
+    public static class DataConstants
+    {
+        public const int BrandNameMaxLength = 50;
+        public const int BrandNameMinLength = 2;
+    }
+}
--- AutoPakevApp.Infrastructure/Data/Models/Brand.cs
+++ AutoPakevApp.Infrastructure/Data/Models/Brand.cs
@@ -1,12 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using static AutoPakevApp.Infrastructure.Constants.DataConstants;
 
 namespace AutoPakevApp.Infrastructure.Data.Models
 {
+    [Comment("Car brand")]
     public class Brand
     {
+        [Key]
+        [Comment("Brand Identifier")]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(BrandNameMaxLength)]
+        [Comment("Brand name")]
+        public string Name { get; set; } = string.Empty;
+
+
+        public List<CarModel> CarModels { get; set; } = new List<CarModel>();
     }
 }
