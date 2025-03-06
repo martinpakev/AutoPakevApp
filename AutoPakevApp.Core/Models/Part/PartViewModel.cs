07ad5bda8818363af8c088a2d1d53a8557c2307b
martinpakev <marti_pakev@abv.bg>
2025-03-06 18:41:15 +0200

    All Parts Page Created

---
+++ AutoPakevApp.Core/Models/Part/PartViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AutoPakevApp.Core.Models.Part
+{
+    public class PartViewModel
+    {
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        public decimal Price { get; set; }
+
+        [Required]
+        public string Category = string.Empty;
+    }
+}
---
+++ AutoPakevApp/Controllers/PartController.cs
@@ -0,0 +1,24 @@
+﻿using AutoPakevApp.Core.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AutoPakevApp.Controllers
+{
+    public class PartController : Controller
+    {
+        private readonly IPartService partService;
+
+        public PartController(IPartService partService)
+        {
+            this.partService = partService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> All()
+        {
+            var model = await partService.AllAsync();
+
+
+            return View(model);
+        }
+    }
+}
