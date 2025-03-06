0f34c577fba74cda35fd6e2012250b9aeb523ac5
martinpakev <marti_pakev@abv.bg>
2025-03-06 18:41:55 +0200

    ClaimsPrincipalExtension Added

---
+++ AutoPakevApp/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Security.Claims;
+
+namespace AutoPakevApp.Extensions
+{
+    public static class ClaimsPrincipalExtensions
+    {
+        public static string Id(this ClaimsPrincipal user)
+        {
+            return user.FindFirstValue(ClaimTypes.NameIdentifier);
+        }
+    }
+}
